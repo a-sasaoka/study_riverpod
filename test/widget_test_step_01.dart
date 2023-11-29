77f0f73b99f55e29c661c92e0acd17c58e32a8a3
a-sasaoka <chikezo@gmail.com>
2023-11-29 09:19:40 +0900

    Widgetテストのパス誤りを修正

--- test/widget_test_step_01.dart
+++ test/widget_test_step_01.dart
@@ -8,7 +8,7 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:study_riverpod/main.dart';
+import 'package:study_riverpod/step_01/main.dart';
 
 void main() {
   testWidgets('Widget表示テスト', (WidgetTester tester) async {
