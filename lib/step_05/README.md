b2ee14cf443e641240ff2c78ba70ebdf9bfd8813
a-sasaoka <chikezo@gmail.com>
2023-11-29 19:52:15 +0900

    ref.listenでProviderを購読する

---
+++ lib/step_05/README.md
@@ -0,0 +1,7 @@
+# study_riverpod
+
+Riverpod学習用のリポジトリ.
+
+### ref.listenでProviderを購読する
+
+* `ref.listen`でProviderの監視とそのハンドリングが可能になる
---
+++ test/step_05_widget_test.dart
@@ -0,0 +1,38 @@
+// This is a basic Flutter widget test.
+//
+// To perform an interaction with a widget in your test, use the WidgetTester
+// utility in the flutter_test package. For example, you can send tap and scroll
+// gestures. You can also use WidgetTester to find child widgets in the widget
+// tree, read text, and verify that the values of widget properties are correct.
+
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+import 'package:study_riverpod/step_05/main.dart';
+
+void main() {
+  testWidgets('Widget動作テスト', (WidgetTester tester) async {
+    // Widgetの構築
+    await tester.pumpWidget(
+      const ProviderScope(
+        child: MyApp(),
+      ),
+    );
+
+    // 初期表示
+    expect(find.text('0'), findsOneWidget);
+
+    // 「+」を2回クリックした後の確認
+    await tester.tap(find.text('+'));
+    await tester.tap(find.text('+'));
+    await tester.pump();
+    expect(find.text('5の倍数になったよ！'), findsNothing);
+
+    // 「+」をさらに3回クリックした後の確認
+    await tester.tap(find.text('+'));
+    await tester.tap(find.text('+'));
+    await tester.tap(find.text('+'));
+    await tester.pump();
+    expect(find.text('5の倍数になったよ！'), findsOneWidget);
+  });
+}
