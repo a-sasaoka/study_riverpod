3aaf51ed66af907f111852242a370d3a25d40c5a
a-sasaoka <chikezo@gmail.com>
2024-01-31 20:50:01 +0900

    不要なインポートを削除

--- lib/step_13/main.dart
+++ lib/step_13/main.dart
@@ -2,7 +2,6 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:intl/intl.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 /* 
